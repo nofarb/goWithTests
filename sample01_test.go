3a654213b817948bbb9c0a446e14d7b4466a3757
nofarb <nofarb@gmail.com>
2020-07-21 11:03:13 -0400

    Update sample01_test.go

--- sample01_test.go
+++ sample01_test.go
@@ -11,3 +11,11 @@ func TestHelloWorld(t *testing.T) {
 		t.Errorf("actual %v\nwant %v", actual, expected)
 	}
 }
+
+func failTestHelloWorld(t *testing.T) {
+	actual := HelloWorld("hoge1")
+	expected := "hello world, hoge"
+	if actual != expected {
+		t.Errorf("actual %v\nwant %v", actual, expected)
+	}
+}
